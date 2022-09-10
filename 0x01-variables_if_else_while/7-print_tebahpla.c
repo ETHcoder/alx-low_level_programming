48fb746c8f2a0d7b60981476cf3af1bcd93e6281
ETHcoder <kaleabma@gmail.com>
2022-09-10 11:46:15 -0700

    add

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 1 - 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		printf(i);
+	}
+	putchar('\n');
+	return (0);
+}
