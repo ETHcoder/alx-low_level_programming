1871a479f3569f4c99f1a9845fc7859395f46f84
ETHcoder <kaleabma@gmail.com>
2022-09-10 12:06:52 -0700

    add

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 00 - 89
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i,e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+
+	}
+	putchar('\n');
+	return (0);
+}
